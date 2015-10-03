7934e5af05e1c4bf0122dd41168939a32c16699b
Tayum <lunnuymag@gmail.com>
2015-10-03 09:08:05 +0300

    switch2 compiled

---
+++ courses/prog_base/self/switch2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int a = 0;
+	scanf("%d", &a);
+	switch (a) {
+	case 1:
+		printf("WEDNESDAY") ; break;
+	case 2:
+		printf("THURSDAY"); break;
+	case 3:
+		printf("FRIDAY"); break;
+	case 4:
+		printf("SATURDAY"); break;
+	case 5:
+		printf("SUNDAY"); break;
+	case 6:
+		printf("MONDAY"); break;
+	case 7:
+		printf("TUESDAY"); break;
+	case 8:
+		printf("WEDNESDAY"); break;
+	case 9:
+		printf("THURSDAY"); break;
+	case 10:
+		printf("FRIDAY"); break;
+	case 11:
+		printf("SATURDAY"); break;
+	case 12:
+		printf("SUNDAY"); break;
+	case 13:
+		printf("MONDAY"); break;
+	case 14:
+		printf("TUESDAY"); break;
+	case 15:
+		printf("WEDNESDAY"); break;
+	case 16:
+		printf("THURSDAY"); break;
+	case 17:
+		printf("FRIDAY"); break;
+	case 18:
+		printf("SATURDAY"); break;
+	case 19:
+		printf("SUNDAY"); break;
+	case 20:
+		printf("MONDAY"); break;
+	case 21:
+		printf("TUESDAY"); break;
+	case 22:
+		printf("WEDNESDAY"); break;
+	case 23:
+		printf("THURSDAY"); break;
+	case 24:
+		printf("FRIDAY"); break;
+	case 25:
+		printf("SATURDAY"); break;
+	case 26:
+		printf("SUNDAY"); break;
+	case 27:
+		printf("MONDAY"); break;
+	case 28:
+		printf("TUESDAY"); break;
+	case 29:
+		printf("WEDNESDAY"); break;
+	case 30:
+		printf("THURSDAY"); break;
+	case 31:
+		printf("FRIDAY"); break;
+	default:
+		return 1;
+	}
+	return 0;
+}
